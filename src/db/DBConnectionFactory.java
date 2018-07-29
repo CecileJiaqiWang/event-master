68734155346c6a2419106f889e6f5532441c1dad
CecileJiaqiWang <jw81@rice.edu>
2018-07-29 00:44:47 -0500

    update DBConnectionFactory

--- src/db/DBConnectionFactory.java
+++ src/db/DBConnectionFactory.java
@@ -1,12 +1,14 @@
 package db;
 
+import db.mysql.MySQLConnection;
+
 public class DBConnectionFactory {
 	private static final String DEFAULT_DB = "mysql";
 	
 	public static DBConnection getConnection(String db) {
 		switch(db) {
 		case "mysql":
-			return null;
+			return new MySQLConnection();
 		case "mongodb":
 			return null;
 		default:
