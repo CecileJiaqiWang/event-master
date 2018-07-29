eff6dec45fa5a4698721776348dab83c6a89e02a
CecileJiaqiWang <jw81@rice.edu>
2018-07-29 01:55:02 -0500

    add JSONObject parser

--- src/rpc/RpcHelper.java
+++ src/rpc/RpcHelper.java
@@ -1,43 +1,78 @@
 package rpc;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class RpcHelper {
-	// Writes a JSONObject to http response.
-	public static void writeJsonObject(HttpServletResponse response, JSONObject obj) throws IOException{
+
+	/**
+	 * Parses a JSONObject from an http request.
+	 * 
+	 * @param request
+	 * @return JSONObject
+	 */
+	public static JSONObject readJsonObject(HttpServletRequest request) {
+		StringBuilder sb = new StringBuilder();
+		try (BufferedReader reader = request.getReader()) {
+			String curr = null;
+			while ((curr = reader.readLine()) != null) {
+				sb.append(curr);
+			}
+			return new JSONObject(sb.toString());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return new JSONObject();
+	}
+
+	/**
+	 * Writes a JSONObject to http response.
+	 * 
+	 * @param response
+	 * @param obj
+	 * @throws IOException
+	 */
+	public static void writeJsonObject(HttpServletResponse response, JSONObject obj) throws IOException {
 		PrintWriter out = response.getWriter();
 		try {
 			response.setContentType("application/json");
 			response.addHeader("Access-Control-Allow-Origin", "*");
 			out.println(obj);
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			out.close();
 		}
-		
-		
+
 	}
 
-	// Writes a JSONArray to http response.
+	/**
+	 * Writes a JSONArray to http response.
+	 * 
+	 * @param response
+	 * @param array
+	 * @throws IOException
+	 */
 	public static void writeJsonArray(HttpServletResponse response, JSONArray array) throws IOException {
 		PrintWriter out = response.getWriter();
 		try {
 			response.setContentType("application/json");
 			response.addHeader("Access-Control-Allow-Origin", "*");
 			out.println(array);
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			out.close();
 		}
-		
 
 	}
 
